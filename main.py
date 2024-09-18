365ec44463d16325c0b835f126e0f90de15b939d
Matthieu Corageoud <matthieu.corageoud@gmail.com>
2024-09-18 23:25:48 +0200

    Update the user agent used to perform the HTTP request

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 URL = "https://www.migrosbank.ch/fr/personnes-privees/prets-hypothecaires/taux-hypothecaires-actuels.html"
 HEADERS = {
-	"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0"
+	"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:130.0) Gecko/20100101 Firefox/130.0"
 }
 
 SCOPE = [
